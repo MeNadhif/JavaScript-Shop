e10e2cd2f9b40df387a38fbeb6b1ee5b37e56e58
NF-Goo <nadhif1495@gmail.com>
2021-03-01 19:02:26 +0700

    fix misslyped class + complete component parts/footer

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import Header from 'parts/Header';
 import Hero from 'parts/Hero';
-import MostPicked from 'parts/MostPicked'
-import Categories from 'parts/Categories'
+import MostPicked from 'parts/MostPicked';
+import Categories from 'parts/Categories';
+import Testimony from 'parts/Testimony';
+import Footer from 'parts/Footer';
 import landingPage from 'json/landingPage.json';
 
 export default class LandingPage extends Component {
@@ -20,6 +22,8 @@ export default class LandingPage extends Component {
           data={landingPage.mostPicked} 
         />
         <Categories data={landingPage.categories} />
+        <Testimony data={landingPage.testimonial} />
+        <Footer />
         
       </>
     )
